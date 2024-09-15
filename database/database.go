2afd7d44045612194be00eaa66c8c1b0ba928b50
vangdevops <vangdevops@gmail.com>
2024-09-15 10:21:36 +0300

    0.0.0

---
+++ database/database.go
@@ -0,0 +1,68 @@
+package database 
+
+import (
+	"os"
+	"strings"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	DB *sql.DB
+	err error
+	version string
+)
+
+func DatabaseConnect(connection string) error {
+	DB, err = sql.Open("mysql", connection)
+	if err != nil {
+		return err
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetVersion() (string,error) {
+	err = DB.QueryRow("SELECT VERSION()").Scan(&version)
+	if err != nil {
+		return "",err
+	}
+	return version,nil
+}
+
+func CheckTable(table string) (bool,error) {
+	table, err := DB.Query("SHOW TABLES LIKE '%s'",table)
+	if err != nil {
+		return false,err
+	}
+	defer res.Close()
+
+	var table string
+	if res.Next() {
+		if err := res.Scan(&table); err != nil {
+			return false,err
+		}
+		return true, nil //found
+	}
+	return false, nil // not found
+}
+
+func CreateTable(table string) error {
+	_,err := DB.Query("CREATE TABLE " + table + "(test INT)")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteTable(table string) error {
+	_,err := DB.Query("DROP TABLE " + table)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ git/git.go
@@ -0,0 +1,69 @@
+package git
+
+import (
+	"net/http"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"time"
+)
+
+func Commit(name string,email string,directory string,commit string) error {
+	r,err := git.PlainOpen(directory)
+	if err != nil {
+		return err
+	}
+	w,err := r.Worktree()
+	if err != nil {
+		return err
+	}
+	_,err = w.Add(directory)
+	if err != nil {
+		return nil
+	}
+	_,err = w.Commit(commit, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  name,
+			Email: email,
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Push(directory) error {
+	r,err := git.PlainOpen(directory)
+	if err != nil {
+		return err
+	}
+	err = r.Push(&git.PushOptions{})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Jenkins(username string,password string,jenurl string,job string,key string) (string,error) {
+	client := http.Client{}
+
+	URL := jenurl+ "/job/"+ job + "/build?token=" + key
+	req, err := http.NewRequest(http.MethodGet, URL, http.NoBody)
+	if err != nil {
+		return "",err
+	}
+
+	req.SetBasicAuth(username, password)
+
+	resp,err = client.Do(req)
+	if err != nil {
+		return "",err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", err
+	}
+	return resp.Status,nil
+}
