dd06e06565631924955a9b50ce0817184dbbbcf6
vangdevops <vangdevops@gmail.com>
2024-09-19 22:31:05 +0300

    jenkins log test

--- git/git_test.go
+++ git/git_test.go
@@ -7,80 +7,80 @@ import (
 )
 
 func TestJenkins(t *testing.T) {
-        t.Run("Success", func(t *testing.T) {
-                server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                        if r.URL.Path == "/job/test-job/build" && r.Method == http.MethodGet {
-                                w.WriteHeader(http.StatusOK)
-                        }
-                }))
-                defer server.Close()
+	t.Run("Success", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/job/test-job/build" && r.Method == http.MethodGet {
+				w.WriteHeader(http.StatusOK)
+			}
+		}))
+		defer server.Close()
 
-                err := Jenkins("user", "pass", server.URL, "test-job", "secret-token")
-                if err != nil {
-                        t.Errorf("Expected no error, but got %v", err)
-                }
-        })
+		err := Jenkins("user", "pass", server.URL, "test-job", "secret-token")
+		if err != nil {
+			t.Errorf("Expected no error, but got %v", err)
+		}
+	})
 
-        t.Run("Unauthorized", func(t *testing.T) {
-                server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                        w.WriteHeader(http.StatusUnauthorized)
-                }))
-                defer server.Close()
+	t.Run("Unauthorized", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusUnauthorized)
+		}))
+		defer server.Close()
 
-                err := Jenkins("user", "pass", server.URL, "test-job", "secret-token")
-                if err == nil || err.Error() != "authentication failed: invalid credentials" {
-                        t.Errorf("Expected authentication failed error, but got %v", err)
-                }
-        })
+		err := Jenkins("user", "pass", server.URL, "test-job", "secret-token")
+		if err == nil || err.Error() != "authentication failed: invalid credentials" {
+			t.Errorf("Expected authentication failed error, but got %v", err)
+		}
+	})
 
-        t.Run("Job Not Found", func(t *testing.T) {
-                server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                        w.WriteHeader(http.StatusNotFound)
-                }))
-                defer server.Close()
+	t.Run("Job Not Found", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer server.Close()
 
-                err := Jenkins("user", "pass", server.URL, "non-existent-job", "secret-token")
-                if err == nil || err.Error() != "job not found: check the job name and URL" {
-                        t.Errorf("Expected job not found error, but got %v", err)
-                }
-        })
+		err := Jenkins("user", "pass", server.URL, "non-existent-job", "secret-token")
+		if err == nil || err.Error() != "job not found: check the job name and URL" {
+			t.Errorf("Expected job not found error, but got %v", err)
+		}
+	})
 }
 
 func TestJenkinsLog(t *testing.T){
 	t.Run("Success",func (t *testing.T){
-                server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                        if r.URL.Path == "/job/test-job/build" && r.Method == http.MethodGet {
-                                w.WriteHeader(http.StatusOK)
-                        }
-                }))
-                defer server.Close()
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/job/test-job/lastBuild/consoleText" && r.Method == http.MethodGet {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte("Hello"))
+			}
+		}))
+		defer server.Close()
 
-                log,err := JenkinsLog("user", "pass", server.URL, "test-job", "secret-token")
-                if err != nil {
-                        t.Errorf("Expected no error, but got %v", err)
-                }
+		_,err := JenkinsLog("user", "pass", server.URL, "test-job")
+		if err != nil {
+			t.Errorf("Expected no error, but got %v", err)
+		}
 	})
 	t.Run("Unauthorized",func (t *testing.T){
-                server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                        w.WriteHeader(http.StatusUnauthorized)
-                }))
-                defer server.Close()
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusUnauthorized)
+		}))
+		defer server.Close()
 
-                err := Jenkins("user", "pass", server.URL, "test-job", "secret-token")
-                if err == nil || err.Error() != "authentication failed: invalid credentials" {
-                        t.Errorf("Expected authentication failed error, but got %v", err)
-                }
-        })
+		_,err := JenkinsLog("user", "pass", server.URL, "test-job")
+		if err == nil || err.Error() != "authentication failed: invalid credentials" {
+			t.Errorf("Expected authentication failed error, but got %v", err)
+		}
+	})
 	t.Run("Job_Not_Found",func(t *testing.T){
-		
-                server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                        w.WriteHeader(http.StatusNotFound)
-                }))
-                defer server.Close()
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusNotFound)
+		}))
+		defer server.Close()
 
-                err := Jenkins("user", "pass", server.URL, "non-existent-job", "secret-token")
-                if err == nil || err.Error() != "job not found: check the job name and URL" {
-                        t.Errorf("Expected job not found error, but got %v", err)
-                }
+		_,err := JenkinsLog("user", "pass", server.URL, "non-existent-job")
+		if err == nil || err.Error() != "job not found: check the job name and URL" {
+			t.Errorf("Expected job not found error, but got %v", err)
+		}
 	})
 }
